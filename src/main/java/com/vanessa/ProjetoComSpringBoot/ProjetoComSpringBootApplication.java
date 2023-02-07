b6cf30500f0bafae503edd3463e2468de00a62d4
Vanessa Machado <vanessamachado@ibm.com>
2023-02-07 18:56:25 -0300

    Projeto criado

---
+++ src/main/java/com/vanessa/ProjetoComSpringBoot/ProjetoComSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.vanessa.ProjetoComSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoComSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoComSpringBootApplication.class, args);
+	}
+
+}
